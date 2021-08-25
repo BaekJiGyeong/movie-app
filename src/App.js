c25af397284f7206d74bc57052ba1f15d391869b
BaekJiGyeong <wlrud0126@hanmail.net>
2021-08-25 22:29:29 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}) { // props.fav
+  return (<div>
+<h3>I like {name } </h3>
+<img src={picture}/>
+  </div>
+    
+     
+  );
+}
+const foodILike = [{name:"kimchi", image:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.maangchi.com%2Frecipe%2Ftongbaechu-kimchi&psig=AOvVaw2fyp3190a-i6DNWaB8mIeD&ust=1629983415924000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKjjlcafzPICFQAAAAAdAAAAABAD"}
+, {name:"chcha", image:"https://img.sbs.co.kr/newimg/news/20200923/201474086_1280.jpg"}];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div> 
+    <h1>Hello!!</h1>
+      {foodILike.map(dish=> (<Food name={dish.name} picture={dish.image}/>))}
+    
     </div>
-  );
+    );
 }
 
 export default App;

4c0944430e25c2d05a99fe7373c517ac94ab6540
BaekJiGyeong <wlrud0126@hanmail.net>
2021-09-10 00:01:35 +0900

    modify detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -20,7 +20,22 @@ class Detail extends React.Component{
         const {location} = this.props;
         if(location.state){
             
-            return <span>{location.state.title}</span>
+            return (
+                <div className="movie">
+                <img src={location.state.poster} alt={location.state.title} title={location.state.title}></img>
+                <div className="movie__data">
+                    <h3 className="movie__title">{location.state.title}</h3>
+                    <h5 className="movie__year">{location.state.year}</h5>
+                    <p className="movie__summary">{location.state.summary.slice(0,180)}...</p>
+                    <ul className="movies__genres">
+    
+                        {location.state.genres.map((genre, index) => 
+                        <li key = {index} className="genres__genre">{index}, {genre}</li>
+                        )}   
+                    </ul>
+                </div>
+            </div>
+            )
         }
         else return null;
     }
